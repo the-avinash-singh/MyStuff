e3d4e434d49837a9456a1c6edc6aecf791d50cec
the-avinash-singh <abhinavarya87012@gmail.com>
2024-01-15 20:15:34 +0530

    resolving error

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,21 @@ app.use(
     origin: "*",
   })
 );
-app.options('*', cors())
 //passing the middlewear
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+if (req.method == "OPTIONS") {
+  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+  return res.status(200).json({});
+}
 
+next();
+});
 //here different routes used to choose specified backend-file for different end points.
 app.use("/", require("./routes/welcome"));
 app.use("/api/auth", require("./routes/auth"));
