1bf5cee9d1a99f46876fba72b42fae75e36cbd5d
the-avinash-singh <abhinavarya87012@gmail.com>
2024-01-15 20:43:33 +0530

    resolving error

--- backend/index.js
+++ backend/index.js
@@ -10,27 +10,12 @@ const app = express();
 const port = 5000;
 //resolving cors policy error
 app.use(
-  cors({
-    origin: "*",
-  })
+  cors()
 );
 //passing the middlewear
 app.use(express.json());
 
 //here different routes used to choose specified backend-file for different end points.
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // You can replace '*' with specific origins if needed
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-  // Allow preflight requests to pass
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
-
 app.use("/", require("./routes/welcome"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
