18e73c99b21c2cd2e97470d1ee69a94320b7cd1a
the-avinash-singh <abhinavarya87012@gmail.com>
2024-01-15 20:11:49 +0530

    resolving error

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const fetchuser = require('../middleware/fetchuser');
-const cors = require("../middleware/corsHandller")
 const Note = require('../models/Note');
 const { body, validationResult } = require('express-validator');
 
 // ROUTE 1: Get All the Notes using: GET "/api/note/getuser". Login required
-router.get('/fetchallnotes',cors, fetchuser, async (req, res) => {
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try {
         const notes = await Note.find({ user: req.user.id });
         res.json(notes)
@@ -17,7 +16,7 @@ router.get('/fetchallnotes',cors, fetchuser, async (req, res) => {
 })
 
 // ROUTE 2: Add a new Note using: POST "/api/note/addnote". Login required
-router.post('/addnote',cors, fetchuser, [
+router.post('/addnote', fetchuser, [
     body('title', 'Enter a valid title').isLength({ min: 2 }),//validations using express validator
     body('description', 'Description must be atleast 5 characters').isLength({ min: 2 }),], async (req, res) => {
         try {
@@ -43,7 +42,7 @@ router.post('/addnote',cors, fetchuser, [
     })
 //ROUTE 3:to update a note:PUT"/api/notes/updatenote/id" . login required
 
-router.put("/updatenote/:id",cors,fetchuser,async (req,res)=>{
+router.put("/updatenote/:id", fetchuser,async (req,res)=>{
 const {title,description,tag}=req.body;
 try {
 //create a new note object
