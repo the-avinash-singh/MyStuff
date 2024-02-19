b9f6d019c9aa4c165f49ed7936b0836c2a087932
Avinash singh <106461441+the-avinash-singh@users.noreply.github.com>
2024-02-19 19:51:28 +0530

    Updates

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -7,7 +7,7 @@ const { body, validationResult } = require('express-validator');
 // ROUTE 1: Get All the Notes using: GET "/api/note/getuser". Login required
 router.get('/fetchallnotes', fetchuser, async (req, res) => {
     try {
-        const notes = await Note.find({ user: req.user.id }).sort({date:-1});
+        const notes = await Note.find({ user: req.user.id }).sort({date:1});
         res.json(notes)
     } catch (error) {
         console.error(error.message);
