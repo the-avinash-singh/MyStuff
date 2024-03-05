a04ae7302fb14eaae4d06c48859e8a1fc6ce7938
the-avinash-singh <abhinavarya87012@gmail.com>
2024-03-05 19:13:07 +0530

    added api

--- src/component/Username.js
+++ src/component/Username.js
@@ -4,7 +4,7 @@ const Username = () => {
   return (
     <div className='container mb-5' >
       <span className='fw-semibold text-light fs-1'>Welcome to Mystuffsafe:</span>
- {/* <span className="fs-2 text-light-emphasis d-inline-flex text-wrap mx-2">{(localStorage.getItem('name'))[0].toUpperCase()+localStorage.getItem('name').slice(1)}</span> */}
+ <span className="fs-2 text-light-emphasis d-inline-flex text-wrap mx-2">{(localStorage.getItem('name'))[0].toUpperCase()+localStorage.getItem('name').slice(1)}</span>
     </div>
   )
 }
